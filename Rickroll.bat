8fd0b0dbe1be9a218ac80b132b852952c772a970
AdamxzV3 <99908796+AdamxzV3@users.noreply.github.com>
2022-10-20 19:28:21 +0200

    Create Rickroll.bat

---
+++ Rickroll.bat
@@ -0,0 +1,10 @@
+@echo off
+Title You just got rickrolled
+color D
+:loop
+Echo You just runned a scary rickroll how do you feel man..
+start chrome.exe>> seturl "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+start msedge.exe>> seturl "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+start firefox.exe>> seturl "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+goto loop
+pause
